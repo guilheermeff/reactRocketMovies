47ddda9a2639860398794a4b373448333c7a108c
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-29 21:35:56 -0400

    add colors and font

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,15 @@
 export default {
   COLORS: {
+    BACKGROUND_100: 'hsl(255, 5%, 15%)',
+    BACKGROUND_INPUT: 'hsl(255, 5%, 15%)',
+    BACKGROUND_FRAME: 'hsl(258, 10%, 20%)',
 
+    PINK: 'hsl(349, 100%, 76%)',
+
+    WHITE_100: 'hsl(273, 10%, 79%)',
+    WHITE_200: 'hsl(25, 35%, 93%)',
+
+    GRAY: 'hsl(270, 5%, 58%)'
   },
 
   FONTS: {
