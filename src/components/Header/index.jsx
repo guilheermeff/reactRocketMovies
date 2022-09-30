04b30d4be16da00a5e7095f6cf5da9b5a14283e6
Guilherme Fernandes <guilheermeff@outlook.com>
2022-09-30 19:14:37 -0400

    finished component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,20 @@
-import { Container } from "./styles";
+import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
   return(
     <Container>
-      
+      <h1>RocketMovies</h1>
+
+      <input type="text" placeholder="Pesquisar pelo título"/>
+
+      <Profile>
+        <div>
+          <strong>Guilherme Fernandes</strong>
+          <Logout>Sair</Logout>
+        </div>
+ 
+        <img src="https://github.com/guilheermeff.png" alt="Imagem do usuário" />
+      </Profile>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,76 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  width: 100%;
+  max-width: 133.6rem;
+  height: 11.6rem;
+
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > h1 {
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.COLORS.PINK};
+
+    margin-left: 12.3rem;
+  }
+
+  > input {
+    width: 100%;
+    max-width: 63.0rem;
+    padding: 1.9rem 2.4rem;
+
+    margin-inline: 6.4rem;
+
+    border: 0;
+    border-radius: 1.0rem;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_200};
+
+    font-size: 1.4rem;
+
+    color: ${({ theme }) => theme.COLORS.GRAY};
+
+    &::placeholder {
+      color: ${({ theme }) => theme.COLORS.GRAY};
+    }
+
+    &:focus {
+      outline: 0;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+
+  display: flex;
+  align-items: center;
+  gap: 1.0rem;
+
+  margin-right: 12.3rem;
+
+  > img {
+    width: 6.4rem;
+  }
+
+  > div {
+
+    text-align: right;
+
+    strong {
+      color: ${({ theme }) => theme.COLORS.WHITE_200};
+      font-size: 1.4rem;
+      line-height: 1.8rem;
+      display: block;
+    }
+  }
+`;
+
+export const Logout = styled.button`
+  border: none;
+  background: none;
+
+  color: ${({ theme }) => theme.COLORS.GRAY};
 `;
