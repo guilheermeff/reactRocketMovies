41034c0f62dcb29e31fffeb4ccbc5c049ca3b862
Guilherme Fernandes <guilheermeff@outlook.com>
2022-11-26 18:28:36 -0400

    added inputs

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,9 @@
 import { Container, Form, Background, Return } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { Link } from 'react-router-dom';
 
 
 export function SignIn() {
@@ -30,7 +32,7 @@ export function SignIn() {
         <Button title="Entrar" />
 
         <Return>
-          <a href="#">Criar conta</a>
+          <Link to="/register">Criar conta</Link>
         </Return>
       </Form>
       <Background />
